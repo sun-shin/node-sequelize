dbe091041719e85ee0b0835e8fbc6df77b804156
sun-shin <sunhanshin2@gmail.com>
2020-12-22 19:20:03 -0600

    adds professors CRUD(index and show functional) and review index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require("./queries");
 const app = express();
 const port = 3000;
 
@@ -9,3 +10,19 @@ app.use(
     extended: true,
   })
 );
+
+// app.get("/", (req, res) => {
+//   res.json({ info: "Node.js, Express, and Postgres API" });
+// });
+
+app.get("/professors", db.getProfessors);
+app.get("/professors/:id", db.getProfessorById);
+app.post("/professors", db.createProfessor);
+// app.put("/professors/:id", db.updateProfessor);
+// app.delete("/professors/:id", db.deleteProfessor);
+
+app.get("/reviews", db.getReviews);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
--- queries.js
+++ queries.js
@@ -1,8 +1,94 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "me",
+  user: "sshin2",
   host: "localhost",
   database: "ratemyprofessor",
   password: "password",
   port: 5432,
 });
+
+// Professors CRUD
+// Professors Index
+const getProfessors = (req, res) => {
+  pool.query("SELECT * FROM professors ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+// Professors Show
+const getProfessorById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query(
+    "SELECT * FROM professors WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
+    }
+  );
+};
+// Professors Create
+const createProfessor = (request, response) => {
+  const { name, university, department } = request.body;
+
+  pool.query(
+    "INSERT INTO professors (name, university, department) VALUES ($1, $2, $3)",
+    [name, university, department],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`Professor added with ID: ${results.insertId}`);
+    }
+  );
+};
+// //Professors Update
+// const updateProfessor = (request, response) => {
+//   const id = parseInt(request.params.id);
+//   const { name, university, department } = request.body;
+
+//   pool.query(
+//     "UPDATE professors SET name = $1, university = $2, department = $3 WHERE id = $4",
+//     [name, university, department, id],
+//     (error, results) => {
+//       if (error) {
+//         throw error;
+//       }
+//       response.status(200).send(`Professor modified with ID: ${id}`);
+//     }
+//   );
+// };
+// //Professors Delete
+// const deleteProfessor = (request, response) => {
+//   const id = parseInt(request.params.id);
+
+//   pool.query("DELETE FROM professors WHERE id = $1", [id], (error, results) => {
+//     if (error) {
+//       throw error;
+//     }
+//     response.status(200).send(`Professor deleted with ID: ${id}`);
+//   });
+// };
+// Reviews CRUD
+// Reviews Index
+const getReviews = (req, res) => {
+  pool.query("SELECT * FROM reviews ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+module.exports = {
+  getProfessors,
+  getProfessorById,
+  createProfessor,
+  // updateProfessor,
+  // deleteProfessor,
+  getReviews,
+};
